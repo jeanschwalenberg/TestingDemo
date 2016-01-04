22f518e01c8a30f381a3bf7afc927cdf0aad5707
jeanschwalenberg <jeanschwalenberg@gmail.com>
2016-01-04 13:40:39 -0700

    Initial Commit:
    AdminController.cs
    User.cs

---
+++ TestingDemo/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestingDemo.Controllers
+{
+    public class AdminController : Controller {
+        private IUserRepository repository;
+
+        public AdminController(IUserRepository repo) {
+            repository = repo;
+        }
+
+        public ActionResult ChangeLoginName(string oldName, string newName) {
+            User user = repository.FetchByLoginName(oldName);
+            user.LoginName = newName;
+            repository.SubmitChanges();
+            //render some view to show result
+            return View();
+}
+    }
+}
---
+++ TestingDemo/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestingDemo.Models
+{
+    public class User {
+        public string LoginName { get; set; }
+    }
+}
