55e472a19c14c4232c4f6dd4943652928a77f8aa
jeanschwalenberg <jeanschwalenberg@gmail.com>
2016-01-04 14:14:33 -0700

    added IUserRepository.cs
    added DefaultUserRepository.cs

---
+++ TestingDemo/Repositories/DefaultUserRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TestingDemo.Repositories;
+using TestingDemo.Models;
+
+namespace TestingDemo.Repositories
+{
+    public class DefaultUserRepository : IUserRepository {
+
+        public void Add(User newUser) {
+            //implement me
+        }
+
+        public User FetchByLoginName(string loginName) {
+            //implement me
+            return new User() { LoginName = loginName };
+        }
+
+        public void SubmitChanges() {
+            //implement me
+        }
+    }
+}
---
+++ TestingDemo/Repositories/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TestingDemo.Models;
+
+namespace TestingDemo.Repositories
+{
+    public interface IUserRepository {
+        void Add(User newUser);
+        User FetchByLoginName(string loginName);
+        void SubmitChanges();
+    }
+}
