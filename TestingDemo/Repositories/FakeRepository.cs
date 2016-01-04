1a564f027b24c6e6dc525d75b33002bd9a5b31c2
jeanschwalenberg <jeanschwalenberg@gmail.com>
2016-01-04 15:50:17 -0700

    added FakeRepository.cs

---
+++ TestingDemo/Repositories/FakeRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TestingDemo.Models;
+using TestingDemo.Repositories;
+
+namespace TestingDemo.Repositories
+{
+    class FakeRepository : IUserRepository {
+        public List<User> Users = new List<User>();
+        public bool DidSubmitChanges = false;
+
+        public void Add(User user) {
+            Users.Add(user);
+        }
+
+        public User FetchByLoginName(string loginName) {
+            return Users.First(m => m.LoginName == loginName);
+        }
+
+        public void SubmitChanges() {
+            DidSubmitChanges = true;
+        }
+    }
+}
