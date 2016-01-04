43e28fa745a105d3a5c1608525cf7350160dcc3b
jeanschwalenberg <jeanschwalenberg@gmail.com>
2016-01-04 15:31:38 -0700

    added "Tests" as folder for this round
    added AdminControllerTests.cs

---
+++ TestingDemo/Tests/AdminControllerTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestingDemo.Models;
+using TestingDemo.Controllers;
+using TestingDemo.Repositories;
+
+namespace TestingDemo.Tests
+{
+    [TestClass]
+    public class AdminControllerTests {
+        [TestMethod]
+        public void CanChangeLoginName() {
+            //Arrange (set up a scenario)
+            User user = new User() { LoginName = "Bob" };
+            FakeRespository repositoryParam = new FakeRespository();
+            repositoryParam.Add(user);
+            AdminController target = new AdminController(repositoryParam);
+            string oldLoginParam = user.LoginName;
+            string newLoginParam = "Joe";
+            //Act (attempt the operation)
+            target.ChangeLoginName(oldLoginParam, newLoginParam);
+            //Assert (verify the result)
+            Assert.AreEqual(newLoginParam, user.LoginName);
+            Assert.IsTrue(repositoryParam.DidSubmitChanges);
+        }
+    }
+}
